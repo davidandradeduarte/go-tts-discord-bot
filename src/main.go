9cc132b8c6620719e71ddaeedcaa745bbfba5ad3
David Duarte <david.duarte@mindera.com>
2021-02-06 21:46:32 +0000

    fixing typo

--- src/main.go
+++ src/main.go
@@ -61,7 +61,7 @@ func main() {
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Airhorn is now running.  Press CTRL-C to exit.")
+	fmt.Println("TTS Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
